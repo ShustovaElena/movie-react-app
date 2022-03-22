99010860453c29f24377a16ea101edda53738bbb
Elena Shustova <alenyf-ka@mail.ru>
2022-03-22 10:56:42 +0300

    feat: create search

--- components/my-app/src/pages/home-page.tsx
+++ components/my-app/src/pages/home-page.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
 
 class Home extends React.Component {
+  state = {
+    userInput: '',
+  };
+
+  handleChange = (event: React.FormEvent) => {
+    const input = event.target as HTMLInputElement;
+
+    this.setState({ userInput: input.value });
+  };
+
+  handleFormSubmit = () => {
+    const userInput = this.state.userInput;
+    localStorage.setItem('userInput', userInput);
+  };
+
+  componentWillUnmount() {
+    const userInput = this.state.userInput;
+    localStorage.setItem('userInput', userInput);
+  }
+
+  componentDidMount() {
+    const userInput = localStorage.getItem('userInput');
+    this.setState({ userInput: userInput });
+  }
+
   render() {
     return (
       <>
         <main>
           <h2>Welcome to the homepage!</h2>
+          <Search />
         </main>
       </>
     );
@@ -13,3 +39,22 @@ class Home extends React.Component {
 }
 
 export default Home;
+
+class Search extends Home {
+  render() {
+    return (
+      <form className="Search" onSubmit={this.handleFormSubmit}>
+        <input
+          className="Search-input"
+          type="text"
+          placeholder="Search"
+          value={this.state.userInput}
+          onChange={this.handleChange}
+        />
+        <button className="Search-btn" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
